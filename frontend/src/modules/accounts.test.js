9096697b3baa6173d84e1747e7d87fd6d16b4c8b
gjstoychev <g.j.stoychev@gmail.com>
2017-07-11 14:50:17 +0300

    wbank: fixed unit tests, added travis continuous integration
    Added Travis Continuous Integration to the project
    A few minor changes adding Spec to unify all unit tests

--- frontend/src/modules/accounts.test.js
+++ frontend/src/modules/accounts.test.js
@@ -1,82 +1,85 @@
 // Accounts Reducer
+
 import reducer from './accounts'
 
-// Tests
-test('should attach accounts to state after they are loaded', () => {
-    const state = {accounts: [], accountDetails: undefined}
+describe('accounts reducer', () => {
+    it('should attach accounts to state after they are loaded', () => {
+        const state = { accounts: [], accountDetails: undefined }
 
-    expect(reducer(state, action('LOAD_ACCOUNTS_SUCCESS', [{AccountID: "1"}, {AccountID: "2"}])))
-        .toEqual(                
+        expect(reducer(state, action('LOAD_ACCOUNTS_SUCCESS', [{ AccountID: "1" }, { AccountID: "2" }])))
+            .toEqual(
             {
-                accounts: [{AccountID: "1"}, {AccountID: "2"}],
+                accounts: [{ AccountID: "1" }, { AccountID: "2" }],
                 accountDetails: undefined
             }
-        )
-})
+            )
+    })
 
-test('should start loading of account details', () => {
-    const state = {accounts: [{AccountID: "1"}, {AccountID: "2"}], accountDetails: undefined}
+    it('should start loading of account details', () => {
+        const state = { accounts: [{ AccountID: "1" }, { AccountID: "2" }], accountDetails: undefined }
 
-    expect(reducer(state, action('LOAD_ACCOUNT_DETAILS', {AccountID: "2", Amount: 23})))
-        .toEqual(
+        expect(reducer(state, action('LOAD_ACCOUNT_DETAILS', { AccountID: "2", Amount: 23 })))
+            .toEqual(
             {
-                accounts: [{AccountID: "1"}, {AccountID: "2"}],
+                accounts: [{ AccountID: "1" }, { AccountID: "2" }],
                 accountDetails: undefined,
                 loading: true
             }
-        )
-})
+            )
+    })
 
-test('should load account details', () => {
-    const state = {accounts: [{AccountID: "1"}, {AccountID: "2"}], accountDetails: undefined}
+    it('should load account details', () => {
+        const state = { accounts: [{ AccountID: "1" }, { AccountID: "2" }], accountDetails: undefined }
 
-    expect(reducer(state, action('LOAD_ACCOUNT_DETAILS_SUCCESS', {AccountID: "2", Amount: 23})))
-        .toEqual(
+        expect(reducer(state, action('LOAD_ACCOUNT_DETAILS_SUCCESS', { AccountID: "2", Amount: 23 })))
+            .toEqual(
             {
-                accounts: [{AccountID: "1"}, {AccountID: "2"}],
-                accountDetails: {AccountID: "2", Amount: 23},
+                accounts: [{ AccountID: "1" }, { AccountID: "2" }],
+                accountDetails: { AccountID: "2", Amount: 23 },
                 loading: false
             }
-        )
-})
+            )
+    })
 
-test('should create new account', () => {
-    const state = {accounts: [{AccountID: "1"}, {AccountID: "2"}], accountDetails: undefined}
+    it('should create new account', () => {
+        const state = { accounts: [{ AccountID: "1" }, { AccountID: "2" }], accountDetails: undefined }
 
-    expect(reducer(state, action('CREATE_ACCOUNT_SUCCESS', {AccountID: "3"})))
-        .toEqual(
+        expect(reducer(state, action('CREATE_ACCOUNT_SUCCESS', { AccountID: "3" })))
+            .toEqual(
             {
-                accounts: [{AccountID: "1"}, {AccountID: "2"}, {AccountID: "3"}],
+                accounts: [{ AccountID: "1" }, { AccountID: "2" }, { AccountID: "3" }],
                 accountDetails: undefined
             }
-        )
+            )
 
-})
+    })
 
-test('should deposit a given amount into selected account and update both accounts and accountDetails', () => {
-    const state = {accounts: [{AccountID: "1", Amount: 100}, {AccountID: "2", Amount: 200}], accountDetails: {AccountID: "1", Amount: 100}}
+    it('should deposit a given amount into selected account and update both accounts and accountDetails', () => {
+        const state = { accounts: [{ AccountID: "1", Amount: 100 }, { AccountID: "2", Amount: 200 }], accountDetails: { AccountID: "1", Amount: 100 } }
 
-    expect(reducer(state, action('DEPOSIT_ACCOUNT_SUCCESS', {AccountID: "1", Amount: 150})))
-        .toEqual(
+        expect(reducer(state, action('DEPOSIT_ACCOUNT_SUCCESS', { AccountID: "1", Amount: 150 })))
+            .toEqual(
             {
-                accounts: [{AccountID: "1", Amount: 150}, {AccountID: "2", Amount: 200}],
-                accountDetails: {AccountID: "1", Amount: 150}
+                accounts: [{ AccountID: "1", Amount: 150 }, { AccountID: "2", Amount: 200 }],
+                accountDetails: { AccountID: "1", Amount: 150 }
             }
-        )
-})
+            )
+    })
 
-test('should withdraw a given amount from selected account and update both accounts and accountDetails', () => {
-    const state = {accounts: [{AccountID: "1", Amount: 100}, {AccountID: "2", Amount: 200}], accountDetails: {AccountID: "2", Amount: 200}}
+    it('should withdraw a given amount from selected account and update both accounts and accountDetails', () => {
+        const state = { accounts: [{ AccountID: "1", Amount: 100 }, { AccountID: "2", Amount: 200 }], accountDetails: { AccountID: "2", Amount: 200 } }
 
-    expect(reducer(state, action('WITHDRAW_ACCOUNT_SUCCESS', {AccountID: "2", Amount: 0})))
-        .toEqual(
+        expect(reducer(state, action('WITHDRAW_ACCOUNT_SUCCESS', { AccountID: "2", Amount: 0 })))
+            .toEqual(
             {
-                accounts: [{AccountID: "1", Amount: 100}, {AccountID: "2", Amount: 0}],
-                accountDetails: {AccountID: "2", Amount: 0}
+                accounts: [{ AccountID: "1", Amount: 100 }, { AccountID: "2", Amount: 0 }],
+                accountDetails: { AccountID: "2", Amount: 0 }
             }
-        )
-})
+            )
+    })
 
-const action = (name, data) => {
-    return {type: name, payload: {data: data}}
-}
+    const action = (name, data) => {
+        return { type: name, payload: { data: data } }
+    }
+    
+})
--- frontend/src/modules/auth.test.js
+++ frontend/src/modules/auth.test.js
@@ -1,49 +1,52 @@
 // Auth Reducer
+
 import reducer from './auth'
 
-// Tests
-test('should change isLoggedIn boolean to false and attach a message to state', () => {
-    const state = {isLoggedIn: false}
+describe('auth reducer', () => {
+    it('should change isLoggedIn boolean to false and attach a message to state', () => {
+        const state = { isLoggedIn: false }
 
-    expect(reducer(state, action('REGISTER_USER_SUCCESS')))
-        .toEqual({isLoggedIn: false, message: 'Registration successful'})
+        expect(reducer(state, action('REGISTER_USER_SUCCESS')))
+            .toEqual({ isLoggedIn: false, message: 'Registration successful' })
+    })
+
+    it('should change isLoggedIn boolean to true and attach a message to state', () => {
+        const state = { isLoggedIn: false }
+
+        expect(reducer(state, action('LOGIN_SUCCESS')))
+            .toEqual({ isLoggedIn: true, message: 'Loggin successful' })
+    })
+
+    it('should change isLoggedIn boolean to false and attach a message to state', () => {
+        const state = { isLoggedIn: true }
+
+        expect(reducer(state, action('LOGOUT_SUCCESS')))
+            .toEqual({ isLoggedIn: false, message: 'Logged out' })
+    })
+
+    it('should keep isLoggedIn boolean true and attach a message to state', () => {
+        const state = {}
+
+        expect(reducer(state, action('LOGOUT_FAIL')))
+            .toEqual({ isLoggedIn: false, message: 'Already logged out' })
+    })
+
+    it('should keep isLoggedIn boolean true and attach a message to state', () => {
+        const state = {}
+
+        expect(reducer(state, action('LOGGED_IN')))
+            .toEqual({ isLoggedIn: true, message: 'Already logged in' })
+    })
+
+    it('should reset the state auth message', () => {
+        const state = { message: 'Already logged in' }
+
+        expect(reducer(state, action('RESET_AUTH_MESSAGE')))
+            .toEqual({ message: '' })
+    })
+
+    const action = (name, data) => {
+        return { type: name, payload: { data: data } }
+    }
+    
 })
-
-test('should change isLoggedIn boolean to true and attach a message to state', () => {
-    const state = {isLoggedIn: false}
-
-    expect(reducer(state, action('LOGIN_SUCCESS')))
-        .toEqual({isLoggedIn: true, message: 'Loggin successful'})
-})
-
-test('should change isLoggedIn boolean to false and attach a message to state', () => {
-    const state = {isLoggedIn: true}
-
-    expect(reducer(state, action('LOGOUT_SUCCESS')))
-        .toEqual({isLoggedIn: false, message: 'Logged out'})
-})
-
-test('should keep isLoggedIn boolean true and attach a message to state', () => {
-    const state = {}
-
-    expect(reducer(state, action('LOGOUT_FAIL')))
-        .toEqual({isLoggedIn: false, message: 'Already logged out'})
-})
-
-test('should keep isLoggedIn boolean true and attach a message to state', () => {
-    const state = {}
-
-    expect(reducer(state, action('LOGGED_IN')))
-        .toEqual({isLoggedIn: true, message: 'Already logged in'})
-})
-
-test('should reset the state auth message', () => {
-    const state = {message: 'Already logged in'}
-
-    expect(reducer(state, action('RESET_AUTH_MESSAGE')))
-        .toEqual({message: ''})
-})
-
-const action = (name, data) => {
-    return {type: name, payload: {data: data}}
-}
